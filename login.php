a3010e2eabd333b76c77a97c97a590e9ce99ce8a
shravyass2003@gmail.com <shravyass2003@gmail.com>
2023-05-06 22:15:46 +0530

    user-login created

--- login.php
+++ login.php
@@ -10,30 +10,36 @@ if(isset($_POST['user_name']) && isset($_POST['password'])) {
     }
     $user_name = validate($_POST['user_name']);
     $password = validate($_POST['password']);
+    if (empty($user_name)) {
+        echo "User Name is required";
+        exit();
+    }else if(empty($password)){
+        echo "Password is required";
+        exit();
+    }else{
         $sql = "SELECT * FROM user WHERE user_name='$user_name' AND password='$password'";
         $result = mysqli_query($conn, $sql);
         if (mysqli_num_rows($result) === 1) {
             $row = mysqli_fetch_assoc($result);
             if ($row['user_name'] === $user_name && $row['password'] === $password) {
-                echo '<script>alert("login successfully ")</script>';
-                $_SESSION['user_name'] = $row['user_name'];
-                $_SESSION['email'] = $row['email'];
-               // header("Location: user-dashboard.html");
+                echo "Logged in!";
+                $_SESSION['userName'] = $row['user_name'];
+                header("Location: user-dashboard.html");
                 exit();
             }else{
-               // header("Location: ../../HTML/sign-in.php?error=Incorect User name or password");
-                echo '<script>alert("Incorrect username or password")</script>';
+                echo "Incorect User name or password";
+                //header("Location: ../../HTML/sign-in.php?error=Incorect User name or password");
+                //echo '<script>alert("Incorrect username or password")</script>';
                 exit();
             }
-        }//else{
-           // header("Location: ../../HTML/sign-in.php?error=Incorect User name or password");
-           // echo '<script>alert("Incorrect username or password")</script>';
-           // exit();
-       // }
+        }else{
+            echo "Incorect User name or password";
+            //header("Location: ../../HTML/sign-in.php?error=Incorect User name or password");
+            //echo '<script>alert("Incorrect username or password")</script>';
+            exit();
+        }
     }
-//else{
-   // header("Location: ../../HTML/sign-up.html");    
-   // exit();
-//}
-?>
-
+}else{
+    //header("Location: ../../HTML/sign-up.html");    
+    exit();
+}
